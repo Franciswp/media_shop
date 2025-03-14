1659e7e1aa9a34fa55796a7d9ccafeb4643d0c78
Chigozie Francis <franksoft1@yahoo.com>
2025-03-14 13:13:06 +0100

    OPI changes

--- src/main/java/com/fcwebapp/mediaproducts/MediaProductsApplication.java
+++ src/main/java/com/fcwebapp/mediaproducts/MediaProductsApplication.java
@@ -1,9 +1,24 @@
 package com.fcwebapp.mediaproducts;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@OpenAPIDefinition(
+        info = @Info(title = "Media product REST API",
+        description = "Media Product REST API for the Media Shop",
+        version = "1.0",
+        contact = @Contact(name = "Francis Chigozie",
+        email = "cyriacus1210@gmail.com",
+        url = "https://franciswebapp.com"),
+        license = @License(
+                name = "Apache 2.0"
+        ))
+)
 public class MediaProductsApplication {
 
     public static void main(String[] args) {
